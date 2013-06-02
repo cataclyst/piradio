6195d1b4c726316ed5d21def334c4f6dbc014070
Andre Lohrenz <mail@cataclyst.de>
2013-06-02 22:32:59 +0200

    More debug messages and error proofness.

--- PiRadio.py
+++ PiRadio.py
@@ -5,18 +5,30 @@ import gaugette.rgbled
 import threading
 import time
 import math
+import sys
 
+# Pins used by the rotary encoder
 ROT_A_PIN = 5
 ROT_B_PIN = 4
 
-SWITCH_PIN = 8
+# Pin used by the push button
+BUTTON_PIN = 8
 
+# Pins used by the LEDs
 R_LED_PIN = 2
 G_LED_PIN = 0
 B_LED_PIN = 1
 
-# Set to True to enable some debug output:
-DEBUG = False
+# Colors (in red, green, blue) to use for the modes
+COLOR_VOLUME = [100,   0,   0]
+COLOR_TRACKS = [0,   100,   0]
+COLOR_OFF    = [0,     0,   0]
+
+# Initial volume in percent
+INITIAL_VOLUME = 80
+
+# Factor in percent to add to the volume for a delta of 1:
+VOLUME_FACTOR = 0.2
 
 # Duration to interpret as a "long" button press (in ms)
 LONG_PRESS_DURATION = 1200
@@ -24,19 +36,21 @@ LONG_PRESS_DURATION = 1200
 # Amount to turn the rotator before a track/station is changed:
 TRACK_ROTATION_THRESHOLD = 20
 
+# Set to True to enable some debug output:
+DEBUG = False
+
 class PiRadio:
   
   def __init__(self):
-    print "A"
-    
+  
     # Mode distincts between 'VOLUME', 'TRACKS' and 'OFF'
-    self.mode = 'VOLUME'
+    self.mode = "VOLUME"
     
     # MPD client connection for playback
     self.playback = mpd.MPDClient()
     
     # Convenience and performance improvement: store the last volume
-    self.last_volume = 80
+    self.last_volume = INITIAL_VOLUME
     
     # Remember a rotation value for changing the track:
     self.last_track_rotation = 0
@@ -65,9 +79,10 @@ class PiRadio:
       while True:
         time.sleep(10)
     except KeyboardInterrupt:
-      print "Bye!"
+      print("Shutting down threads")
       rotator_thread.stop()
       button_thread.stop()
+      print("Threads shut down")
     except:
       rotator_thread.stop()
       button_thread.stop()
@@ -75,54 +90,73 @@ class PiRadio:
     
   def rotator_changed(self, delta):
     # Depending on the current mode, do something:
+    
+    # If in "change volume" mode, add a certain factor of the delta to the volume:
     if self.mode == "VOLUME":
-      self.last_volume += (delta * 0.2)
-      if (self.last_volume > 100):
-        self.last_volume = 100
-      print self.last_volume
+      self.last_volume += (delta * VOLUME_FACTOR)
+      # Clamp the volume between 0 and 100:
+      self.last_volume = min(100, max(0, self.last_volume))
+      if DEBUG:
+        print("Volume: %3.1f" % self.last_volume)
       self.playback.setvol(int(self.last_volume))
+      
+    # If in "change tracks" mode, count rotations until a change of track is reached:
     elif self.mode == "TRACKS":
       self.last_track_rotation += delta
-      print self.last_track_rotation
+      if DEBUG:
+        print("Last track rotation: %d" % self.last_track_rotation)
       if abs(self.last_track_rotation) > TRACK_ROTATION_THRESHOLD:
         if math.copysign(1, self.last_track_rotation) < 0:
+          if DEBUG:
+            print("Going back to previous track")
           self.playback.previous()
         else:
+          if DEBUG:
+            print("Advancing to next track")
           self.playback.next()
         self.last_track_rotation = 0
 
   def button_released(self):
     # Depending on the current mode, react to the button release:
-    print "Button released"
+    if DEBUG:
+      print("Button released")
     if self.mode == "VOLUME":
       self.mode = "TRACKS"
+      if DEBUG:
+        print("Mode changed to 'TRACKS'")
     else:
       self.mode = "VOLUME"
+      if DEBUG:
+        print("Mode changed to 'VOLUME'")
     self.adapt_led()
     
   def button_long_press(self):
     # Depending on the current mode, a long press on the button turns playback off or on:
-    print "Button long-press!"
     if self.mode == "OFF":
+      if DEBUG:
+        print("Mode changed to 'VOLUME' (turning back on)")
       self.mode = "VOLUME"
       self.playback.play()
     else:
       self.mode = "OFF"
+      if DEBUG:
+        print("Mode changed to 'OFF'")
       self.playback.stop()
     self.adapt_led()
     
   def adapt_led(self):
     if self.mode == "VOLUME":
-      colors = [100, 0, 0]
+      colors = COLOR_VOLUME
     elif self.mode == "TRACKS":
-      colors = [0, 100, 0]
+      colors = COLOR_TRACKS
     elif self.mode == "OFF":
-      colors = [0, 0, 0]
+      colors = COLOR_OFF
       
     if self.mode == "OFF":
-      self.led.set(colors[0], colors[1], colors[2])
+      f = self.led.set
     else:
-      self.led.fade(colors[0], colors[1], colors[2])
+      f = self.led.fade
+    f(colors[0], colors[1], colors[2])
     
 class RotatorThread(threading.Thread): 
   def __init__(self, pi_radio): 
@@ -146,7 +180,8 @@ class RotatorThread(threading.Thread):
       if delta != 0:
         self.master.rotator_changed(delta)
       time.sleep(0.01)
-    print "RotatorThread stopped"
+    if DEBUG:
+      print("RotatorThread stopped")
       
 class ButtonThread(threading.Thread): 
   def __init__(self, pi_radio): 
@@ -156,7 +191,7 @@ class ButtonThread(threading.Thread):
     self.master = pi_radio
     
     # Push button to change between modes and to stop and start playback
-    self.switch = gaugette.switch.Switch(SWITCH_PIN)
+    self.switch = gaugette.switch.Switch(BUTTON_PIN)
     
     # Indicator for the thread to finish:
     self.stop_requested = False
@@ -194,11 +229,17 @@ class ButtonThread(threading.Thread):
           long_press_registered = True
           time_of_press = -1
       time.sleep(0.01)
+    if DEBUG:
+      print("ButtonThread stopped")
       
 if __name__ == "__main__":
-  print "PiRadio wird gestartet..."
-  radio = PiRadio()
-  print "finishing"
+  if len(sys.argv) == 2 and sys.argv[1].lower() == "debug":
+    DEBUG = True
+  if DEBUG:
+    print("Starting PiRadio...")
+  PiRadio()
+  if DEBUG:
+    print("PiRadio finished")
   
   
   
